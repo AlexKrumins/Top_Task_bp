6c71ec9f2be177f11698c3f20e4e67862d10e389
Alex Krumins <justkrumins@gmail.com>
2019-07-27 16:55:58 -0500

    adding HList

---
+++ client/src/components/HList/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Droppable } from 'react-beautiful-dnd';
+
+const grid = 8;
+
+const getHListStyle = isDraggingOver => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  display: 'flex',
+  padding: grid,
+  overflow: 'auto',
+});
+
+function HList(props) {
+  return(
+    <Droppable droppableId={props.droppableId} direction="horizontal">
+      {(provided, snapshot) => (
+        <div
+        ref={provided.innerRef}
+        style={getHListStyle(snapshot.isDraggingOver)}>
+          {props.children}
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
+  )
+}
+export default HList;
