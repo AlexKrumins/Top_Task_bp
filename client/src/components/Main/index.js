2d537e68a94c2b52522f079b03ba4fedc658f6f3
Alex <justkrumins@gmail.com>
2019-08-03 03:44:38 -0500

    adding stupid components

---
+++ client/src/components/Main/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Droppable } from 'react-beautiful-dnd';
+
+
+const grid = 8;
+
+const getListStyle = isDraggingOver => ({
+    background: isDraggingOver ? 'lightblue' : 'lightgrey',
+    padding: grid,
+    border: 2
+});
+
+function List(props) {
+  return(
+    <Droppable  droppableId={props.droppableId} isDropDisabled={props.isDropDisabled} >
+      {(provided, snapshot) => (
+        <div
+          ref={provided.innerRef}
+          style={getListStyle(snapshot.isDraggingOver)}>
+            {props.children}
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
+  )
+}
+export default List;
