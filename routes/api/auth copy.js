150e57bcf29d20c64249d1ae961f444bd85ee536
Alex Krumins <justkrumins@gmail.com>
2019-10-28 21:06:12 -0500

    auth copy

---
+++ routes/api/auth copy.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const passport = require('../../config/passport');
+const authController = require('../../controllers/auth');
+
+router.use(passport.authenticate('local'));
+
+// Matches with '/api/auth/login'
+router.route('/login')
+  .post(authController.login);
+
+router.route("/user_data")
+  .get(authController.user_data)
+
+module.exports = router;
